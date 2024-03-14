67886d6b39ccbb3bebdc8fa7f800f984d30a1170
theravirai <theravirai108@gmail.com>
2024-03-14 21:45:00 +0100

    select bachground element, child element...

---
+++ hero-hover-effect/scripts.js
@@ -0,0 +1,4 @@
+const parentBgEl = document.querySelector(".bg");
+const childEls = document.querySelectorAll(".container .text span");
+const textEl = document.querySelector(".text");
+const lineEl = document.querySelector("#line");
