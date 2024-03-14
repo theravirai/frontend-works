732b975a6c1eef350059434c531f93af67a265c1
theravirai <theravirai108@gmail.com>
2024-03-14 19:28:00 +0100

    add event listener, select images...

--- hero-hover-effect/scripts.js
+++ hero-hover-effect/scripts.js
@@ -2,3 +2,18 @@ const parentBgEl = document.querySelector(".bg");
 const childEls = document.querySelectorAll(".container .text span");
 const textEl = document.querySelector(".text");
 const lineEl = document.querySelector("#line");
+
+
+childEls.forEach((el) => {
+    el.addEventListener("mouseover", (e) => {
+      const id = el.getAttribute("data-id");
+      const bgEl = parentBgEl.querySelector(`.id-${id}`);
+  
+      parentBgEl.querySelectorAll("img").forEach((img) => {
+        img.style.display = "none";
+        img.style.animation = "";
+      });
+      bgEl.style.display = "block";
+      bgEl.style.animation = "anima 1s ease forwards";
+    });
+  });
