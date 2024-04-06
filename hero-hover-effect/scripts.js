d6979f4fe6e93fbe242e893bf3b6cadae8003d3b
theravirai <theravirai108@gmail.com>
2024-04-06 20:55:00 +0200

    update scripts.js

--- hero-hover-effect/scripts.js
+++ hero-hover-effect/scripts.js
@@ -3,24 +3,23 @@ const childEls = document.querySelectorAll(".container .text span");
 const textEl = document.querySelector(".text");
 const lineEl = document.querySelector("#line");
 
-
 childEls.forEach((el) => {
     el.addEventListener("mouseover", (e) => {
-      const id = el.getAttribute("data-id");
-      const bgEl = parentBgEl.querySelector(`.id-${id}`);
-  
-      parentBgEl.querySelectorAll("img").forEach((img) => {
-        img.style.display = "none";
-        img.style.animation = "";
-      });
-      bgEl.style.display = "block";
-      bgEl.style.animation = "anima 1s ease forwards";
-  
-      // line animation
-      const cords = el.getBoundingClientRect();
-      const textCords = textEl.getBoundingClientRect();
-  
-      lineEl.style.width = cords.width + "px";
-      lineEl.style.left = cords.left - textCords.left + "px";
+        const id = el.getAttribute("data-id");
+        const bgEl = parentBgEl.querySelector(`.id-${id}`);
+
+        parentBgEl.querySelectorAll("img").forEach((img) => {
+            img.style.display = "none";
+            img.style.animation = "";
+        });
+        bgEl.style.display = "block";
+        bgEl.style.animation = "anima 1s ease forwards";
+
+        // line animation
+        const cords = el.getBoundingClientRect();
+        const textCords = textEl.getBoundingClientRect();
+
+        lineEl.style.width = cords.width + "px";
+        lineEl.style.left = cords.left - textCords.left + "px";
     });
-  });
+});
